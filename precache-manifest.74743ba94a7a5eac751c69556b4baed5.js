ea963806cc2b7b473e8fcda75053b56f6c45d9d7
Rob Donigian <donigianrp@gmail.com>
2020-01-12 17:55:40 -0500

    Updates

---
+++ precache-manifest.74743ba94a7a5eac751c69556b4baed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f8b353b3df7658ea806b78ba3b8a38",
+    "url": "/GuitarReff/index.html"
+  },
+  {
+    "revision": "dfb408763196ae8843dc",
+    "url": "/GuitarReff/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5419fbfbea423254ed4c",
+    "url": "/GuitarReff/static/js/2.e41ecc3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/GuitarReff/static/js/2.e41ecc3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfb408763196ae8843dc",
+    "url": "/GuitarReff/static/js/main.bc01435f.chunk.js"
+  },
+  {
+    "revision": "f2bc203ae936f08465ce",
+    "url": "/GuitarReff/static/js/runtime-main.2110ad2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GuitarReff/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2cca7d6bf0c744dee33e0ef39bbce73.js"
+  "/GuitarReff/precache-manifest.74743ba94a7a5eac751c69556b4baed5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GuitarReff/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
