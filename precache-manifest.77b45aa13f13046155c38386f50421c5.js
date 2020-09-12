3d6abc9ca93f2148cfd03780a35d0a3304adadff
Rob Donigian <donigianrp@gmail.com>
2020-09-12 17:07:27 -0400

    Updates

--- precache-manifest.77b45aa13f13046155c38386f50421c5.js
+++ precache-manifest.77b45aa13f13046155c38386f50421c5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32740bcd3b34d99cb75607a4d9ce1afe",
+    "revision": "9520d25abf1d32bcfb769e3c22a90167",
     "url": "/index.html"
   },
   {
-    "revision": "65af5d803e59f31e6a13",
+    "revision": "02f29883b6bc068fe0c3",
     "url": "/static/css/main.c34adccd.chunk.css"
   },
   {
-    "revision": "7af1275a634ec3724483",
-    "url": "/static/js/2.ac025a60.chunk.js"
+    "revision": "37abdc2ff3da586cfac4",
+    "url": "/static/js/2.81487756.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/static/js/2.ac025a60.chunk.js.LICENSE"
+    "url": "/static/js/2.81487756.chunk.js.LICENSE"
   },
   {
-    "revision": "65af5d803e59f31e6a13",
-    "url": "/static/js/main.4f4d86ab.chunk.js"
+    "revision": "02f29883b6bc068fe0c3",
+    "url": "/static/js/main.bf93976c.chunk.js"
   },
   {
     "revision": "1dffb1d4ea175f35aeeb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c80b3248d55daae1482d8a0cd3fcd01.js"
+  "/precache-manifest.77b45aa13f13046155c38386f50421c5.js"
 );
 
 self.addEventListener('message', (event) => {
